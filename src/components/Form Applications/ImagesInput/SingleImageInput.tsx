3c721988d9442c22243986029e76d71890f3b731
mohanned-banna <mohanned.banna03@gmail.com>
2026-01-08 14:20:08 +0200

    refactory on images in dialog

--- src/components/Form Applications/ImagesInput/SingleImageInput.tsx
+++ src/components/Form Applications/ImagesInput/SingleImageInput.tsx
@@ -1,157 +1,3 @@
-// import { Controller } from "react-hook-form";
-// import {
-//   Card,
-//   CardContent,
-//   Typography,
-//   IconButton,
-//   Box,
-//   Stack,
-// } from "@mui/material";
-// import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import CropIcon from "@mui/icons-material/Crop";
-// import { useRef, useState } from "react";
-// import { ImageCropDialog } from "./ImageCropDialog";
-// import classNames from "classnames";
-
-// const MAX_SIZE = 2 * 1024 * 1024;
-
-// const SingleImageInput = ({
-//   control,
-//   name,
-//   label,
-//   isChangeToReviewPage,
-// }: {
-//   control: any;
-//   name: string;
-//   label: string;
-//   isChangeToReviewPage:boolean
-// }) => (
-//   <Controller
-//     name={name}
-//     control={control}
-//     defaultValue={null}
-//     rules={{
-//       validate: (file: File | null) => {
-//         if (!file) return true;
-//         if (file.size > MAX_SIZE)
-//           return "حجم الصورة يجب أن لا يتجاوز 2MB";
-//         return true;
-//       },
-
-//     }}
-//     render={({ field, fieldState }) => {
-//       const inputRef = useRef<HTMLInputElement | null>(null);
-//       const [preview, setPreview] = useState<string | null>(null);
-//       const [openCrop, setOpenCrop] = useState(false);
-
-//       const setFile = (file: File | null) => {
-//         field.onChange(file);
-//         setPreview(file ? URL.createObjectURL(file) : null);
-
-//         if (inputRef.current) inputRef.current.value = "";
-//       };
-
-//       return (
-//         <Box>
-//           <label className="block text-sm font-medium mb-2">
-//             {label}
-//           </label>
-
-//           <Card
-//             variant="outlined"
-//             sx={{
-//               borderStyle: "dashed",
-//               textAlign: "center",
-//               cursor: "pointer",
-//               bgcolor: "#fafafa",
-//             }}
-//             className={classNames({"pointer-events-none":isChangeToReviewPage})}
-//             onClick={() => inputRef.current?.click()}
-//           >
-//             <CardContent>
-//               <input
-//                 ref={inputRef}
-//                 type="file"
-//                 hidden
-//                 accept="image/*"
-//                 onChange={(e) =>
-//                   setFile(e.target.files?.[0] || null)
-//                 }
-//               />
-
-//               {preview ? (
-//                 <Box position="relative">
-//                   <img
-//                     src={preview}
-//                     style={{
-//                       maxHeight: 160,
-//                       margin: "auto",
-//                       borderRadius: 8,
-//                     }}
-//                   />
-
-//                   <Stack
-//                     direction="row"
-//                     spacing={1}
-//                     sx={{ position: "absolute", top: 6, right: 6 }}
-//                   >
-//                     <IconButton
-//                       size="small"
-//                       sx={{ bgcolor: "primary.main", color: "#fff" }}
-//                       onClick={(e) => {
-//                         e.stopPropagation();
-//                         setOpenCrop(true);
-//                       }}
-//                     >
-//                       <CropIcon fontSize="small" />
-//                     </IconButton>
-
-//                     <IconButton
-//                       size="small"
-//                       sx={{ bgcolor: "error.main", color: "#fff" }}
-//                       onClick={(e) => {
-//                         e.stopPropagation();
-//                         setFile(null);
-//                       }}
-//                     >
-//                       <DeleteIcon fontSize="small" />
-//                     </IconButton>
-//                   </Stack>
-//                 </Box>
-//               ) : (
-//                 <>
-//                   <CloudUploadIcon fontSize="large" color="action" />
-//                   <Typography variant="body2" color="text.secondary">
-//                     اضغط أو اسحب الصورة هنا
-//                   </Typography>
-//                 </>
-//               )}
-//             </CardContent>
-//           </Card>
-
-//           {fieldState.error && (
-//             <Typography color="error" variant="caption">
-//               {fieldState.error.message}
-//             </Typography>
-//           )}
-
-//           {/* Crop Dialog */}
-//           {preview && (
-//             <ImageCropDialog
-//               open={openCrop}
-//               image={preview}
-//               onClose={() => setOpenCrop(false)}
-//               onCropComplete={(file) => setFile(file)}
-//             />
-//           )}
-//         </Box>
-//       );
-//     }}
-//   />
-// );
-
-// export default SingleImageInput;
 
 import { Controller } from "react-hook-form";
 import {
@@ -239,11 +85,13 @@ const SingleImageInput = ({
             {label && (
               <Typography
                 variant="body2"
-                fontWeight="medium"
                 gutterBottom
                 sx={{
                   mb: 1,
                   textAlign: language === "ar" ? "right" : "left",
+                  mr:1,
+                  fontWeight:'bold',
+                  fontSize:15
                 }}
               >
                 {label}
